02f4ced8e50615034219cbde9ef54716a7fb9fa6
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-20 12:22:18 +0700

    Fix indentations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 class Nav extends React.Component { 
   render() {
-return (
-<>
-<nav className="navbar border-bottom">
-<div className="container">
-  <a href="" className="navbar-brand"><img src="../../public/logo-airbnb.png" alt="Airbnb logo" width="100px"/></a>
-  <form className="d-flex ps-5" role="search">
-    <button className="btn btn-outline-secondary ms-2" type="submit">Login</button>
-  </form>
-</div>
-</nav>
-</>) 
-}
+    return (
+      <>
+        <nav className="navbar border-bottom">
+          <div className="container">
+             <a href="" className="navbar-brand"><img src="../../public/logo-airbnb.png" alt="Airbnb logo" width="100px"/></a>
+            <form className="d-flex ps-5" role="search">
+              <button className="btn btn-outline-secondary ms-2" type="submit">Login</button>
+            </form>
+          </div>
+        </nav>
+      </>
+    ) 
+  }
 }
 
 export default Nav
