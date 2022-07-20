ef5c96fb032142c3584efb2a241ad2f1042025e8
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-20 13:13:47 +0700

    Fix Airbnb icons path

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ class Nav extends React.Component {
       <>
         <nav className="navbar border-bottom">
           <div className="container">
-             <a href="" className="navbar-brand"><img src="../../public/logo-airbnb.png" alt="Airbnb logo" width="100px"/></a>
+            <a href="" className="navbar-brand"><img src="./logo-airbnb.png" alt="Airbnb logo" width="100px"/></a>
             <form className="d-flex ps-5" role="search">
               <button className="btn btn-outline-secondary ms-2" type="submit">Login</button>
             </form>
