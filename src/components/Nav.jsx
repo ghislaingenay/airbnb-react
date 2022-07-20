69b97df9446a3cb1eec98dce940ce654cd3d3677
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-20 10:32:15 +0700

    Create Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Nav extends React.Component { 
+  render() {
+return (
+<>
+<nav className="navbar border-bottom">
+<div className="container">
+  <a className="navbar-brand"><img src="/public/logo-airbnb.png" alt="Airbnb logo" width="100px"/></a>
+  <form className="d-flex ps-5" role="search">
+    <button className="btn btn-outline-secondary ms-2" type="submit">Login</button>
+  </form>
+</div>
+</nav>
+</>) 
+}}
+
+export default Nav
